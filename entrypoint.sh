05cbca0ee802f20c38aa35c680dc243e9797df45
Tuschl <tuschl@digitalmobil.com>
2016-11-21 21:00:08 +0100

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+if [ ! -d $COCKPIT_ROOT ]; then
+    mkdir -p $COCKPIT_ROOT
+fi
+
+if test ! -f $COCKPIT_ROOT/COCKPIT_VERSION || "x$(cat $COCKPIT_ROOT/COCKPIT_VERSION)" != $COCKPIT_VERSION; then
+    tar -C $COCKPIT_ROOT -xf /usr/src/cockpit.tar.gz --strip-components 1 
+    echo "$COCKPIT_VERSION" > $COCKPIT_ROOT/COCKPIT_VERSION
+fi
+
+chown -R www-data:www-data $COCKPIT_ROOT
+
+exec "$@"
+
