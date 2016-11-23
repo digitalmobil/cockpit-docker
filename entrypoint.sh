f480d118ddab57052ef0358d1effacd93e1c51e8
Tuschl <tuschl@digitalmobil.com>
2016-11-23 08:12:27 +0100

    Fix version conditional in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ ! -d $COCKPIT_ROOT ]; then
     mkdir -p $COCKPIT_ROOT
 fi
 
-if test ! -f $COCKPIT_ROOT/COCKPIT_VERSION || "x$(cat $COCKPIT_ROOT/COCKPIT_VERSION)" != $COCKPIT_VERSION; then
+if test ! -f $COCKPIT_ROOT/COCKPIT_VERSION || test "x$(cat $COCKPIT_ROOT/COCKPIT_VERSION)" != "x$COCKPIT_VERSION"; then
     tar -C $COCKPIT_ROOT -xf /usr/src/cockpit.tar.gz --strip-components 1 
     echo "$COCKPIT_VERSION" > $COCKPIT_ROOT/COCKPIT_VERSION
 fi
